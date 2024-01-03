9e62da3da8108029f0a5eee49b8232fee87a7c38
vanessa <escavanessa@gmail.com>
2024-01-03 14:40:54 -0500

    added project - no images

--- Components/Projects/Projects.jsx
+++ Components/Projects/Projects.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import './Projects.css'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+      <div class="project-container">
+
+
+        
+        <div class="project-1">
+          <div className='p1'>
+            <p>Job Dashboard</p>
+          </div>
+        </div>
+        <div class="project-2">
+          <div className='p2'>
+            <p>NASA Open API</p>
+          </div>
+        </div>
+        <div class="project-3">
+          <div className='p3'>
+            <p>Little Lemon</p>
+          </div>
+        </div>
+        <div class="project-4">
+          <div className='p4'>
+            <p>NoteBook</p>
+          </div>
+        </div>
+
+
+
+
+        <div class="desc-1">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maiores blanditiis recusandae, cupiditate suscipit fugit molestias magni aliquid deleniti quas. Tenetur aliquid amet quidem, voluptatem officiis libero mollitia at accusamus.
+          </p>
+        </div>
+        <div class="desc-2">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, dignissimos nesciunt molestiae eius accusamus ut dolore. Illo labore dignissimos sapiente asperiores magnam eos porro esse dolorem neque excepturi? Debitis, facere!
+          </p>
+        </div>
+        <div class="desc-3">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore repellat quo tempore neque doloribus quidem veritatis voluptate dolores libero cum vel recusandae rem odit delectus, nam amet non quae aspernatur?
+          </p>
+        </div>
+        <div class="desc-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi aspernatur culpa fuga architecto ipsum, porro expedita dolor blanditiis quisquam quidem, cumque dolore, perspiciatis praesentium beatae quaerat maxime in reiciendis velit?
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
 
