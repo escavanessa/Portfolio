43f95d4330ad1627ce60dc0dfe01a77949390328
vanessa <escavanessa@gmail.com>
2024-01-04 20:28:35 -0500

    this website looks horrible

--- Components/Timeline/Timeline.jsx
+++ Components/Timeline/Timeline.jsx
@@ -10,6 +10,21 @@ const Timeline = () => {
         <div className='box-3'></div>
         <div className='box-4'></div>
         <div className='box-5'></div>
+        <div className='circ-1'></div>
+        <div className='circ-2'></div>
+        <div className='circ-3'></div>
+        <div className='circ-4'></div>
+        <div className='circ-5'></div>
+        <h2 className='num-1'>1</h2>
+        <h2  className='num-2'>2</h2>
+        <h2  className='num-3'>3</h2>
+        <h2  className='num-4'>4</h2>
+        <h2  className='num-5'>5</h2>
+        <p className='info-1'>1. I graduated from an Arts Highschool with a very <br></br>clear vision of what i want for the future</p>
+        <p className='info-2'>2. I graduate from Massachusettes College of Art and Design <br></br>after working in Museums for multiple years</p>
+        <p className='info-3'>3. I paint freelance for some time and enjoy it but <br></br>quickly realize this isnt sustainable; I cant find <br></br>a job in my field. I need to pivot</p>
+        <p className='info-4'>4. I learn to code and dont look back.</p>
+        <p className='info-5'>5. Im building my own projects, focusing on building <br></br> my fundamental programming skills and continue <br></br> to fight for an opportunity in tech.</p>
     </div>
     </>
   )
