de2c95ef3fa7116fb414790c0bca50c739913c3b
vanessa <escavanessa@gmail.com>
2024-01-20 22:06:56 -0500

    css

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './Footer.css'
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <a href='https://github.com/escavanessa'><FaGithub className="f-logo" /></a>
+      <a href='www.linkedin.com/in/vescamilla'><FaLinkedin  className="f-logo"/></a>
+      <FaInstagram  className="f-logo"/>
+    </div>
   )
 }
 
