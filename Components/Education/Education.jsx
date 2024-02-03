0d1946d7593e53a99b89e776391bbb7dc645b620
vanessa <escavanessa@gmail.com>
2024-02-02 20:08:39 -0500

    MORE MOBILE CHANGES

--- Components/Education/Education.jsx
+++ Components/Education/Education.jsx
@@ -10,9 +10,8 @@ const Education = () => {
   return (
     <>
 
-        <div className='action'>
-          <h1>Education:</h1>
-          <h2>Drag to the left for more!</h2>
+        <div className='action' id='education'>
+          <h1>Some of my <span className='action-title'>Education</span></h1>
           <span className='arrow-btn' id='education'><FaArrowCircleRight /><FaArrowCircleDown /></span>
           
         </div>
@@ -39,8 +38,8 @@ const Education = () => {
               <h1>Massachusettes College of Art and Design</h1>
               <h2>Art History | 2018 - 2022</h2>
               <p>
-                I graduated with a degree in Art History along with Honors. I was also a double major in Painting but decided to drop the major my final year. The education prepared me to think abstractly and deeply. I cultivated necessary tools like communication -- add more skills ---
-
+                I graduated with a degree in Art History. I was also a double major in Painting but decided to drop the major my final year. My arts education prepared me to think creatively and approach problems in new ways.
+                Although I am following a different path, the core skills I learned - how to communicate effectively, how to work in a team settings and maintain productivity - will serve me in any path I choose.
               </p>
             </SwiperSlide>
 
@@ -48,7 +47,7 @@ const Education = () => {
               <h1>Coursera Front End Development Certificate</h1>
               <h2>2023 - 2024</h2>
               <p>
-                Although this course by no means was my own resource when learning Front End - it was largely my spring board - pointing me in different directions to dig deeper and sharpen my skills. This course covered the basics like HTML and CSS and basic Javascript and then moved into more advanced topics like state in React, Async functions/ fetching data and ended with a thorough introduction to Data Structures.
+                Although this course by no means was my own resource when learning Front End - it was largely my spring board - pointing me in different directions to dig deeper and sharpen my skills. This course covered the basics like HTML and CSS and basic Javascript and then moved into more advanced topics like state/ useEffect in React, Async functions/ fetching data and ended with a thorough introduction to Data Structures.
               </p>
             </SwiperSlide>
 
@@ -56,14 +55,14 @@ const Education = () => {
               <h1>Codecademy</h1>
               <h2>2023 - 2024</h2>
               <p>
-                Completed two Javascript courses - Javascript and Intermediate Javascript. These courses have really helped me build a strong understanding of Javascript and set the foundation for more complicated concepts I will face as I continue on my course inot Front End Development.
+                Completed two Javascript courses - Javascript and Intermediate Javascript. These courses have really helped me build a strong understanding of Javascript and set the foundation for more complicated concepts I will face as I continue on my study into Front End Development.
               </p>
             </SwiperSlide>
 
             <SwiperSlide className='swiperSlide'>
               <h1>CS50</h1>
-              <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, asperiores?</h2>
-              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe maiores porro nihil maxime suscipit, sint quod deserunt labore obcaecati voluptatem tempora alias debitis adipisci. Veniam vel voluptate molestiae porro voluptatem.</p>
+              <h2>2023 - Present</h2>
+              <p>Who wouldnt want to sit in on Harvard lectures for free? The Computer Science course offered by Harvard was the perfect undertone for my work. As I continue to build projects, these lectures have provided me with the philosophical understanding of computers and its logical applications and approaches.</p>
             </SwiperSlide>
           </Swiper>
         </div>
--- Components/Nav/Nav.jsx
+++ Components/Nav/Nav.jsx
@@ -8,7 +8,7 @@ const Nav = () => {
                 <div className='nav-ul'>
                     <a href='#hero-wrapper' className='nav-li'>Home</a>
                     <a href='#education' className='nav-li'>Education</a>
-                    <a href='#project-container' className='nav-li'>Projects</a>
+                    <a href='#projects' className='nav-li'>Projects</a>
                     <a href='#contact' className='nav-li'>Contact</a>
                 </div>
             </nav>
