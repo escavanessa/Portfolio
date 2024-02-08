5bcf863147a985787f882670f8989bac53292157
vanessa <escavanessa@gmail.com>
2024-02-07 20:55:41 -0500

    some small changes

--- Components/Projects/Projects.jsx
+++ Components/Projects/Projects.jsx
@@ -20,10 +20,13 @@ const Projects = () => {
           </div>
         </div>
         <div class="project-3 animate__animated animate__fadeIn">
-          <div className='p3'>
-            <p>Little Lemon</p>
-          </div>
+          <a href='https://escavanessa.github.io/Little-Lemon/'>
+            <div className='p3'>
+              <p>Little Lemon</p>
+            </div>
+          </a>
         </div>
+
         <div class="project-4 animate__animated animate__fadeIn">
           <div className='p4'>
             <p>NoteBook</p>
@@ -45,7 +48,7 @@ const Projects = () => {
         </div>
         <div class="desc-3">
           <p>
-           The Coursera Capstone project. I used React and Tailwind to build a fully functional reservation system for the Little Lemon website. Its important to note the API Coursera provided did not work so I had no access to data and had to build it myself with the help of a brilliant friend.
+            The Coursera Capstone project. I used React and Tailwind to build a fully functional reservation system for the Little Lemon website. Its important to note the API Coursera provided did not work so I had no access to data and had to build it myself with the help of a brilliant friend.
           </p>
         </div>
         <div class="desc-4">
